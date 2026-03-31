fc4a9b73a63f8e672ddb10b3da44a4d0432d3638
Dididididid1501 <vasiliagleev@gmail.com>
2026-04-01 01:02:16 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('surpriseBtn').addEventListener('click', function() {
+    const message = document.getElementById('surpriseMessage');
+    message.textContent = '✨ Ты нажал на кнопку! Это сюрприз! ✨';
+    message.classList.remove('hidden');
+    message.classList.add('show');
+});
